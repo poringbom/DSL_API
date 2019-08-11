c8bead4797807b586b0d2e77839cc2b6f69d3cf6
Pongchet Amornjarusontikul <pongchet@orcsoft.co.th>
2019-08-11 15:48:13 +0700

    Update security config.

--- src/main/java/th/co/ktb/dsl/config/WebSecurityConfig.java
+++ src/main/java/th/co/ktb/dsl/config/WebSecurityConfig.java
@@ -1,13 +1,31 @@
 package th.co.ktb.dsl.config;
 
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 
+@EnableWebSecurity 
 public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
-	  @Override
-	  protected void configure(HttpSecurity http) throws Exception {
-	    http.requiresChannel()
-	      .requestMatchers(r -> r.getHeader("X-Forwarded-Proto") != null)
-	      .requiresSecure();
-	  }
+	String[] swagger_url = {"/v2/api-docs",
+            "/configuration/ui",
+            "/swagger-resources",
+            "/configuration/security",
+            "/swagger-ui.html",
+            "/webjars/**"};
+	
+	@Override
+	protected void configure(HttpSecurity http) throws Exception {
+		http
+			.csrf().disable()
+			.cors().disable()
+			.requiresChannel()
+			.requestMatchers(r -> r.getHeader("X-Forwarded-Proto") != null)
+			.requiresSecure()
+			.and()
+			.authorizeRequests()
+			.antMatchers("/api/v1/**").permitAll()
+			.anyRequest().authenticated()
+			.and()
+			.formLogin();
+	}
 }
